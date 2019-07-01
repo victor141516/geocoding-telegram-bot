8034ad38b39ef862218d01ae0d69f68027697660
victor141516 <yomellamovictor@gmail.com>
2019-07-01 10:34:12 +0100

    add logging

--- geo.py
+++ geo.py
@@ -1,6 +1,11 @@
+import logging
 import requests
 
 GOOGLE_PLACES_API_URL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 
 def get_coordinates(api_key, search_term):
@@ -10,9 +15,10 @@ def get_coordinates(api_key, search_term):
         "inputtype": "textquery",
         "fields": "formatted_address,name,geometry"
     }
-
+    logging.debug(search_term)
     req = requests.get(GOOGLE_PLACES_API_URL, params=params)
     res = req.json()
+    logging.debug(res)
 
     return [
         {
